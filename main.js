b3159aefa470ebbec957d336e365da1dde119a19
Emilly2-emi <100239936+Emilly2-emi@users.noreply.github.com>
2022-12-27 18:39:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -41,14 +41,14 @@ function draw(){
     document.getElementById("volume").innerHTML = "Volume = " + volume;
     song.setVolume(volume);
 
-    if(scoreLeftWrist > 0.2){
+    /*if(scoreLeftWrist > 0.2){
         circle(leftWristX,leftWristY,20);
         InNumberleftWristY = Number(leftWristY);
         remove_decimals = floor(InNumberleftWristY);
         volume = remove_decimals/500;
         document.getElementById("volume").innerHTML = "Volume = " + volume;
         song.setVolume(volume);
-    }
+    }*/
 }
 
 function play()
